cbf8e3965af48970488c3a23c65adffaa2600f87
Joshua Ethridge <joshua.ethridge13@gmail.com>
2021-12-04 21:52:07 -0500

    Day 1 part 2

--- Day1/Day1.py
+++ Day1/Day1.py
@@ -14,8 +14,30 @@ def part1(path):
             line = f.readline()
     return count
 
+def part2(path):
+    with open(path, 'r') as f:
+        line = f.readline()
+        count = 0
+        prev = None
+        group = []
+        while line:
+            line = int(line)
+            group.append(line)
+            if len(group) == 4:
+                group.pop(0)
+            if len(group) == 3:
+                s = sum(group)
+                if not prev:
+                    prev = s
+                else:
+                    if s > prev:
+                        count += 1
+                prev = s
+            line = f.readline()
+    return count
+
 if __name__ == '__main__':
     # Part 1: 1791
     print(part1('Day1/Day1.txt'))
-    # Part 2:
-    #print(part2())
+    # Part 2: 1822
+    print(part2('Day1/Day1.txt'))
--- Day1/tests.py
+++ Day1/tests.py
@@ -7,5 +7,9 @@ class Test(unittest.TestCase):
     def test1(self):
         self.assertEqual(Day1.part1('Day1/example.txt'), 7)
 
+    # Part 2 Tests
+    def test2(self):
+        self.assertEqual(Day1.part2('Day1/example.txt'), 5)
+
 if __name__ == '__main__':
     unittest.main()
