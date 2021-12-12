d801cf6d8bbd55e11e5767c3d189c5a0fe1622a4
Joshua Ethridge <joshua.ethridge13@gmail.com>
2021-12-11 21:38:34 -0500

    Add template for days

---
+++ Day4/tests.py
@@ -0,0 +1,16 @@
+import Day
+import unittest
+
+class Test(unittest.TestCase):
+
+    # Part 1 Tests
+
+    def test1(self):
+        pass
+
+    # Part 2 Tests
+    def test2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ DayTemplate/Day.py
@@ -0,0 +1,11 @@
+def part1(path):
+    pass
+
+def part2(path):
+    pass
+
+if __name__ == '__main__':
+    # Part 1: 
+    print(part1('Day/input.txt'))
+    # Part 2: 
+    print(part2('Day/input.txt'))
