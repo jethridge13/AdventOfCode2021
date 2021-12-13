7f763cadeebde251af8be5537eae5e1d25897a64
Joshua Ethridge <joshua.ethridge13@gmail.com>
2021-12-13 11:14:03 -0500

    Day 6

--- Day6/Day6.py
+++ Day6/Day6.py
@@ -1,33 +1,38 @@
 class Fish:
 
     def __init__(self, line):
-        self.fish = list(map(int, line.split(',')))
+        fish = list(map(int, line.split(',')))
+        self.fish = [0] * 10
+        for i in fish:
+            self.fish[i] += 1
 
     def simulate(self, n):
         for i in range(n):
             for index, fish in enumerate(self.fish):
-                if fish == 0:
-                    self.fish.append(9)
-                    self.fish[index] = 7
-                self.fish[index] -= 1
+                if index == 0:
+                    self.fish[9] += fish
+                    self.fish[7] += fish
+                else:
+                    self.fish[index-1] = fish
+            self.fish[-1] = 0
 
 def part1(path):
     with open(path, 'r') as f:
         line = f.readline()
         fish = Fish(line)
     fish.simulate(80)
-    return len(fish.fish)
+    return sum(fish.fish)
 
 def part2(path):
     with open(path, 'r') as f:
         line = f.readline()
         fish = Fish(line)
     fish.simulate(256)
-    return len(fish.fish)
+    return sum(fish.fish)
 
 if __name__ == '__main__':
     path = 'Day6/input.txt'
     # Part 1: 379114
     print(part1(path))
-    # Part 2: 
+    # Part 2: 1702631502303
     print(part2(path))
--- Day6/tests.py
+++ Day6/tests.py
@@ -7,13 +7,17 @@ class Test(unittest.TestCase):
 
     def testFishSimulate(self):
         fish = Day6.Fish('3,4,3,1,2')
-        self.assertListEqual(fish.fish, [3,4,3,1,2])
+        self.assertEqual(sum(fish.fish), 5)
+        self.assertListEqual(fish.fish, [0,1,1,2,1,0,0,0,0,0])
         fish.simulate(1)
-        self.assertListEqual(fish.fish, [2,3,2,0,1])
+        self.assertEqual(sum(fish.fish), 5)
+        self.assertListEqual(fish.fish, [1,1,2,1,0,0,0,0,0,0])
         fish.simulate(1)
-        self.assertListEqual(fish.fish, [1,2,1,6,0,8])
+        self.assertEqual(sum(fish.fish), 6)
+        self.assertListEqual(fish.fish, [1,2,1,0,0,0,1,0,1,0])
         fish.simulate(8)
-        self.assertListEqual(fish.fish, [0,1,0,5,6,0,1,2,2,3,7,8])
+        self.assertEqual(sum(fish.fish), 12)
+        self.assertListEqual(fish.fish, [3,2,2,1,0,1,1,1,1,0])
 
     # Part 1 Tests
     def test1(self):
@@ -22,8 +26,7 @@ class Test(unittest.TestCase):
 
     # Part 2 Tests
     def test2(self):
-        # self.assertEqual(Day6.part2(path), 26984457539)
-        pass
+        self.assertEqual(Day6.part2(path), 26984457539)
 
 if __name__ == '__main__':
     unittest.main()
