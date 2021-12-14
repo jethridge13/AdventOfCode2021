51285556cc021ca80e7ce0dcc0dc248621121bfc
Joshua Ethridge <joshua.ethridge13@gmail.com>
2021-12-13 21:56:59 -0500

    Day 9

--- Day9/Day9.py
+++ Day9/Day9.py
@@ -31,17 +31,60 @@ def findLowPoints(grid):
 def calcRisk(lowPoints):
     return sum(lowPoints.values()) + len(lowPoints)
 
+def findBasins(grid, lowPoints):
+    basins = {}
+    for coord in lowPoints:
+        basin = [coord]
+        coordsToCheck = [coord]
+        coordsChecked = {}
+        coordsChecked[coord] = True
+        while coordsToCheck:
+            c = coordsToCheck.pop(0)
+            i = c[0]
+            j = c[1]
+            # North
+            north = (i-1,j)
+            if i > 0 and north not in coordsChecked and grid[i-1][j] != 9:
+                basin.append(north)
+                coordsToCheck.append(north)
+                coordsChecked[north] = True
+            # East
+            east = (i,j-1)
+            if j > 0 and east not in coordsChecked and grid[i][j-1] != 9:
+                basin.append(east)
+                coordsToCheck.append(east)
+                coordsChecked[east] = True
+            # South
+            south = (i+1,j)
+            if i < len(grid)-1 and south not in coordsChecked and grid[i+1][j] != 9:
+                basin.append(south)
+                coordsToCheck.append(south)
+                coordsChecked[south] = True
+            # West
+            west = (i,j+1)
+            if j < len(grid[i])-1 and west not in coordsChecked and grid[i][j+1] != 9:
+                basin.append(west)
+                coordsToCheck.append(west)
+                coordsChecked[west] = True
+        basins[coord] = basin
+    return basins
+
 def part1(path):
     grid = generateGrid(path)
     lowPoints = findLowPoints(grid)
     return calcRisk(lowPoints)
 
 def part2(path):
-    pass
+    grid = generateGrid(path)
+    lowPoints = findLowPoints(grid)
+    basins = findBasins(grid, lowPoints)
+    sortedBasins = list(basins.values())
+    sortedBasins.sort(key=lambda x: len(x))
+    return len(sortedBasins[-1]) * len(sortedBasins[-2]) * len(sortedBasins[-3])
 
 if __name__ == '__main__':
     path = 'Day9/input.txt'
     # Part 1: 456
     print(part1(path))
-    # Part 2: 
+    # Part 2: 1047744
     print(part2(path))
--- Day9/tests.py
+++ Day9/tests.py
@@ -27,8 +27,19 @@ class Test(unittest.TestCase):
         self.assertEqual(Day9.part1(path), 15)
 
     # Part 2 Tests
+    def findBasins(self):
+        grid = [[2,1,9,9,9,4,3,2,1,0],
+            [3,9,8,7,8,9,4,9,2,1],
+            [9,8,5,6,7,8,9,8,9,2],
+            [8,7,6,7,8,9,6,7,8,9],
+            [9,8,9,9,9,6,5,6,7,8]]
+        lowPoints = {(0, 1): 1, (0, 9): 0, (2, 2): 5, (4, 6): 5}
+        basins = Day9.findBasins(grid, lowPoints)
+        self.assertCountEqual(basins[(0,1)], [(0,1),(0,2),(0,3)])
+        self.assertCountEqual(basins[(2,2)], [(2,2),(2,1),(2,3),(2,4),(2,5),(1,2),(1,3),(1,4),(3,2),(3,1),(3,0),(3,3),(3,4),(4,1)])
+
     def test2(self):
-        pass
+        self.assertEqual(Day9.part2(path), 1134)
 
 if __name__ == '__main__':
     unittest.main()
