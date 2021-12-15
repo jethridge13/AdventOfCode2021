25f176429b89f51374a80a4c2ef0c8b6e01a8fa0
Joshua Ethridge <joshua.ethridge13@gmail.com>
2021-12-14 20:40:23 -0500

    Day 12 WIP

---
+++ Day12/Day12.py
@@ -0,0 +1,50 @@
+class Cave:
+
+    def __init__(self, letter):
+        self.name = letter
+        self.isBig = not letter.lower() == self.name
+        self.connections = []
+        self.terminal = letter == 'start' or letter == 'end'
+
+def buildCaveMap(path):
+    caveMap = {}
+    with open(path, 'r') as f:
+        line = f.readline().replace('\n', '')
+        while line:
+            caves = line.split('-')
+            if caves[0] not in caveMap:
+                caveMap[caves[0]] = Cave(caves[0])
+            if caves[1] not in caveMap:
+                caveMap[caves[1]] = Cave(caves[1])
+            caveMap[caves[0]].connections.append(caves[1])
+            caveMap[caves[1]].connections.append(caves[0])
+            line = f.readline().replace('\n', '')
+    return caveMap
+
+def findAllPaths(caveMap, cave='start', path=['start'], paths=[], visited = {'start': True}):
+    for c in caveMap[cave].connections:
+        if c in visited and visited[c]:
+            continue
+        path.append(c)
+        if not caveMap[c].isBig:
+            visited[c] = True
+        if c == 'end':
+            paths.append(path)
+        else:
+            findAllPaths(caveMap, c, path, paths)
+        path.pop()
+    return paths
+
+def part1(path):
+    caveMap = buildCaveMap(path)
+    return len(findAllPaths(caveMap))
+
+def part2(path):
+    pass
+
+if __name__ == '__main__':
+    path = 'Day12/input.txt'
+    # Part 1: 
+    print(part1(path))
+    # Part 2: 
+    print(part2(path))
---
+++ Day12/tests.py
@@ -0,0 +1,47 @@
+import Day12
+import unittest
+
+path = 'Day12/example.txt'
+path2 = 'Day12/example2.txt'
+path3 = 'Day12/example3.txt'
+
+class Test(unittest.TestCase):
+
+    # Part 1 Tests
+    def testBuildCaveMap(self):
+        caveMap = Day12.buildCaveMap(path)
+        self.assertCountEqual(caveMap['start'].connections, ['A', 'b'])
+        self.assertCountEqual(caveMap['end'].connections, ['A', 'b'])
+        self.assertCountEqual(caveMap['A'].connections, ['start', 'b', 'c', 'end'])
+        self.assertCountEqual(caveMap['b'].connections, ['start', 'end', 'A', 'd'])
+        self.assertCountEqual(caveMap['c'].connections, ['A'])
+        self.assertCountEqual(caveMap['d'].connections, ['b'])
+        caveMap = Day12.buildCaveMap(path2)
+        self.assertCountEqual(caveMap['start'].connections, ['HN', 'kj', 'dc'])
+        self.assertCountEqual(caveMap['end'].connections, ['dc','HN'])
+
+    def testFindAllPaths(self):
+        caveMap = Day12.buildCaveMap(path)
+        ans =   [['start','A','b','A','c','A','end'],
+                ['start','A','b','A','end'],
+                ['start','A','b','end'],
+                ['start','A','c','A','b','A','end'],
+                ['start','A','c','A','b','end'],
+                ['start','A','c','A','end'],
+                ['start','A','end'],
+                ['start','b','A','c','A','end'],
+                ['start','b','A','end'],
+                ['start','b','end']]
+        self.assertCountEqual(Day12.findAllPaths(caveMap), ans)
+
+    def test1(self):
+        self.assertEqual(Day12.part1(path), 10)
+        self.assertEqual(Day12.part1(path2), 19)
+        self.assertEqual(Day12.part1(path3), 226)
+
+    # Part 2 Tests
+    def test2(self):
+        pass
+
+if __name__ == '__main__':
+    unittest.main()
